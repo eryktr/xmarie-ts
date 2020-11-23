96a26dc095ac51cfa7d48b679775fabab041fcff
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-11-23 18:40:22 +0100

    removed bash wrapper for starting server

--- runserver.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Server path is $XMARIE_SERVER_PATH"
-cd $XMARIE_SERVER_PATH
-source env/bin/activate
-python -m flask run
--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,13 @@ def xmarie_driver():
 @pytest.fixture(scope='session', autouse=True)
 def backend_server():
     path_to_server_app = os.getenv('XMARIE_SERVER_PATH')
-    p = subprocess.Popen(['bash', 'runserver.sh'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, start_new_session=True)
+    cmd = [f'{path_to_server_app}/env/bin/python', '-m', 'flask', 'run']
+    p = subprocess.Popen(
+        cmd, 
+        cwd=path_to_server_app,
+        stdout=subprocess.DEVNULL, 
+        stderr=subprocess.STDOUT, 
+        start_new_session=True,
+    )
     yield
-    for proc in psutil.process_iter():
-        if proc.name() == "python":
-            proc.kill()
+    p.kill()
